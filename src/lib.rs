f0a119a862c684bae8a5f25468a9d5aa24605127
Joaquin Bejar <jb@taunais.com>
2025-07-03 12:15:14 +0200

    Bump version to `0.1.16` and update references in `README.md` and `src/lib.rs`.

--- src/lib.rs
+++ src/lib.rs
@@ -32,7 +32,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! ig-client = "0.1.15"
+//! ig-client = "0.1.16"
 //! tokio = { version = "1", features = ["full"] }  # For async runtime
 //! dotenv = "0.15"                                 # For environment variable loading
 //! tracing = "0.1"                                # For logging
